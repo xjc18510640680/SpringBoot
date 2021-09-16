15a856b792521c58ca1d97b3008a6c51ca997c8b
xujiucheng <xjc851191369@163.com>
2021-09-16 15:51:25 +0800

    代码更新

--- src/test/java/com/sn/miaosha/io/BufferedReader.java
+++ src/test/java/com/sn/miaosha/io/BufferedReader.java
@@ -1,7 +1,6 @@
 package com.sn.miaosha.io;
 
-import java.io.*;
-import java.math.BigDecimal;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,15 +19,7 @@ public class BufferedReader {
     private static final String pathname = "/Users/xujiucheng/Desktop/work/idea-workspace/GitSelf/miaosha/src/test/java/com/sn/miaosha/io/test.txt";
 
     public static void main(String[] args) {
-        //发送 GET 请求
-//        String s = HttpRequest.sendGet("http://localhost:6144/Home/RequestString", "key=123&v=456");
-//        System.out.println(s);
-
-        //发送 POST 请求
-//        String sr = HttpRequest.sendPost("https://prod-i0-scfcoord.carzone360.com/backdoor/event/retry?", "id=[100000718228473226]");
-//        System.out.println(sr);
-        getIdLists(BufferedReader.getTexts()).forEach(System.out::println);
-        System.out.println(new BigDecimal("25000").subtract(new BigDecimal("499.72").add(new BigDecimal("1500")).add(new BigDecimal("1479.07"))));
+        System.out.println(getIdLists(BufferedReader.getTexts()));
     }
 
     /**
@@ -38,10 +29,7 @@ public class BufferedReader {
         List<String> readLists = new ArrayList<>();
         try {
             System.out.println(pathname);
-            readLists = Files
-                    .lines(Paths.get(pathname), Charset.defaultCharset())
-                    .flatMap(line -> Arrays.stream(line.split("\n")))
-                    .collect(Collectors.toList());
+            readLists = Files.lines(Paths.get(pathname), Charset.defaultCharset()).flatMap(line -> Arrays.stream(line.split("\n"))).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,11 +38,9 @@ public class BufferedReader {
 
     public static List<String> getIdLists(List<String> list) {
         List<String> resultList = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder().append("[");
         list.forEach(idList->{
-            stringBuilder.append(idList.substring(1, idList.indexOf("]"))).append(",");
+            resultList.add(idList.substring(1, idList.indexOf("]")));
         });
-        resultList.add(stringBuilder.append("]").toString());
         return resultList;
     }
 
